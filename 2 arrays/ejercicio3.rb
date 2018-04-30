b104d9954988776851be90ca40d5ebfe648367c9
ex.marin <ex.marinm@gmail.com>
2018-04-30 18:56:09 -0300

    Ejercicio 3 listo

--- 2 arrays/ejercicio3.rb
+++ 2 arrays/ejercicio3.rb
@@ -5,3 +5,34 @@
  # 4. Crear un método que incrementa todos los elementos en una unidad y devuelva un arreglo nuevo.
 
 a = [1,2,3,9,1,4,5,2,3,6,6]
+
+def array_even_ele(array)
+  array.each do
+    array.delete_if { |i| (i % 2).zero? }
+  end
+end
+
+print array_even_ele a
+puts "\n"
+
+def array_sum(array)
+  sum = 0
+  array.each { |i| sum += i }
+  puts sum
+end
+
+array_sum a
+
+def array_mean(array)
+  mean = array.inject { |sum, i| sum + i }.to_f / array.size
+  puts mean
+end
+
+array_mean a
+
+def array_plus_one(array)
+  new_array = array.map { |i| i + 1 }
+  print new_array, "\n"
+end
+
+array_plus_one(a)
