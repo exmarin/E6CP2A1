304955d994b64d498d932e1f3c6d2938b4ae2f20
ex.marin <ex.marinm@gmail.com>
2018-04-30 18:50:03 -0300

    Ejercicio 1 listo

--- 2 arrays/ejercicio1.rb
+++ 2 arrays/ejercicio1.rb
@@ -1,8 +1,13 @@
 # Dado el array:
-# 1. Mostrar en pantalla el primer elemento.
-# 2. Mostrar en pantalla el último elemento.
-# 3. Mostrar en pantalla todos los elementos.
-# 4. Mostrar en pantalla todos los elementos junto con un índice.
-# 5. Mostrar en pantalla todos los elementos que se encuentren en una posición par.
-
 arreglo = [1,2,3,9,1,4,5,2,3,6,6]
+
+# 1. Mostrar en pantalla el primer elemento.
+puts arreglo[0]
+# 2. Mostrar en pantalla el último elemento.
+puts arreglo[10]
+# 3. Mostrar en pantalla todos los elementos.
+puts arreglo
+# 4. Mostrar en pantalla todos los elementos junto con un índice.
+print arreglo
+# 5. Mostrar en pantalla todos los elementos que se encuentren en una posición par.
+puts arreglo[1,4]
